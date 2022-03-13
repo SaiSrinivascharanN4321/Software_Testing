ff9cfe809b04e10a34a8ca10ae1b3645c9554282
sadasiva111 <sadasiva6699@gmail.com>
2022-03-13 19:53:31 +0100

    Initial code for string method application

---
+++ String_Method_Application.py
@@ -0,0 +1,11 @@
+#string methods in python
+class string_methods_application:
+
+    #Task_1
+    #Converts the first letter of the string to capital letter.
+    def capitalize_string(my_string):
+        return my_string.capitalize()
+
+    #Returns true if the start value is a specified value    
+    def startswith_string(my_string):
+        return my_string.startswith("Software")    
