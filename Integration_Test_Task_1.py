fc475d007110395ec4a61091dc2929edd496e3a3
SreeChandan13 <chandu7264530046@gmail.com>
2022-03-13 23:00:48 +0000

    failed test cases added by chandan

--- Integration_Test_Task_1.py
+++ Integration_Test_Task_1.py
@@ -19,5 +19,5 @@ class TestStringMethods(unittest.TestCase):
     #Integration Test of Task_3    
     #Integrating endswith and casefold string methods
     def test_integration_Task_3(self):
-        output = string_methods_application.endswith_string(string_methods_application.casefold_string("SOFTWARE TESTING."))
+        output = string_methods_application.endswith_string(string_methods_application.casefold_string("SOFTWARE TESTING"))
         self.assertTrue(output, ".")
